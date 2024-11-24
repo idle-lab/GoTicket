45a7bb816a67bf81435cdce891e98ab2afcd9bae
hstxmgzY <gooeyerwz@gmail.com>
2024-11-24 20:46:49 +0800

    fix some problem of static pages

--- front/src/components/Bottom/index.js
+++ front/src/components/Bottom/index.js
@@ -1,22 +1,13 @@
 import { Col, Row } from 'antd'
-import './index.css'
-import { AudioOutlined } from '@ant-design/icons'
-import { Input, Typography } from 'antd'
-import { Divider } from 'antd'
-const { Search } = Input
+import { Typography } from 'antd'
 const { Text } = Typography
-
-const onSearch = (value, _e, info) => console.log(info?.source, value)
 
 const Bottom = () => {
   return (
     <div className="container">
       <div className="container-box">
-        <Row>
-          <Col span={4}>
-            <div className="logo">GoTicket</div>
-          </Col>
-          <Col span={4}>
+        <Row type="flex" justify="center" align="middle">
+          <Col span={12} offset={10}>
             <Text>Copyright@GoTicket</Text>
           </Col>
         </Row>
